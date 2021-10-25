a9e2ba5baccff31bc6536cab51bef6ac0cadfb80
Samuel <42661697+SamuelSSan28@users.noreply.github.com>
2021-10-24 21:11:04 -0300

    add products scroll list

--- frontend/src/screens/Product_List/index.js
+++ frontend/src/screens/Product_List/index.js
@@ -1,25 +1,33 @@
 import * as React from 'react';
 import styles from './styles.js';
-import { View } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { List } from 'react-native-paper';
+import {productList} from '../../services/products'
 import ProductItem from '../../components/ProductItem'
 import SearchBox from '../../components/SearchBox';
 
 const ProductList = () => {
   const [expanded, setExpanded] = React.useState(true);
-
   const handlePress = () => setExpanded(!expanded);
 
   return (
     <View style={styles.container}>
-    <SearchBox/> 
-    <List.Section>
-        <ProductItem/>
-        <ProductItem/>
-        <ProductItem/>
-        <ProductItem/>
-        <ProductItem/>    
-    </List.Section>
+      <SearchBox />
+      <List.Section>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={productList}
+          renderItem={({item,index}) => 
+              <ProductItem 
+                Nome={item.Nome} 
+                Quantidade={item.Quantidade} 
+                Preco={item.Preco}/>
+          }
+          keyExtractor={item => String(item.id)}
+          horizontal={false}
+        />
+        
+      </List.Section>
     </View>
   );
 };
